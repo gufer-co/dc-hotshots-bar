3051a98cb060d51d88014bf9dcc62ae1a007fad0
gufer-co <gferme@gmail.com>
2026-06-03 18:59:49 +0200

    Service Worker entfernt - verhinderte Updates auf dem Handy

--- sw.js
+++ sw.js
@@ -1,27 +1,8 @@
-const CACHE = 'darts-bar-v4';
-const ASSETS = ['/index.html', '/manifest.json'];
-
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
-  self.skipWaiting();
-});
-
+// Service Worker deregistriert sich selbst
+self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys =>
-    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-  ));
-  self.clients.claim();
-});
-
-// Network-first: immer aktuell vom Server, Cache nur wenn offline
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    fetch(e.request)
-      .then(res => {
-        const clone = res.clone();
-        caches.open(CACHE).then(c => c.put(e.request, clone));
-        return res;
-      })
-      .catch(() => caches.match(e.request))
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
+      .then(() => self.registration.unregister())
   );
 });
