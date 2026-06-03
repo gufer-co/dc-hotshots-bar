5ad5e6018f73b1a9a809faef40d90f7f54d02721
Guido Ferme <gferme@gmail.com>
2026-06-03 17:59:32 +0200

    DC Hotshots Vereinsbar PWA

---
+++ sw.js
@@ -0,0 +1,27 @@
+const CACHE = 'darts-bar-v3';
+const ASSETS = ['/index.html', '/manifest.json'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+
+// Network-first: immer aktuell vom Server, Cache nur wenn offline
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request)
+      .then(res => {
+        const clone = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, clone));
+        return res;
+      })
+      .catch(() => caches.match(e.request))
+  );
+});
